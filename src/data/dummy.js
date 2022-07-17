8e2be372cd633ad32115a564bcfdb9189ee6ede8
Chiara <chiarapanato22@gmail.com>
2022-07-17 11:45:24 +0200

    Finished impelmenting ecommerce page

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -3084,7 +3084,6 @@ export const dropdownData = [
 ];
 export const SparklineAreaData = [
   { x: 1, yval: 2 },
-  { x: 2, yval: 6 },
   { x: 3, yval: 8 },
   { x: 4, yval: 5 },
   { x: 5, yval: 10 },
